b565f3427b9fcab7e0092462d88dbc5963c5b4c6
casualism <casualism.contact@gmail.com>
2022-09-10 22:24:28 +0800

    Fixed wrong link

--- hubconf.py
+++ hubconf.py
@@ -4,12 +4,14 @@ dependencies = ['torch', 'math']
 
 
 def general_model():
-    checkpoint = 'https://github.com/casualism/minecraft-diffusion/releases/download/v1/better_model.pt'
+    checkpoint = 'https://github.com/casualism/minecraft-diffusion/releases/download/v1/general_model.pt'
     model = ViT(1024, 1000)
-    return model.load_state_dict(load_state_dict_from_url(checkpoint, progress=True))
+    model.load_state_dict(load_state_dict_from_url(checkpoint, progress=True))
+    return model
 
 
 def better_model():
     checkpoint = 'https://github.com/casualism/minecraft-diffusion/releases/download/v1/better_model.pt'
     model = ViT(1024, 1000)
-    return model.load_state_dict(load_state_dict_from_url(checkpoint, progress=True))
+    model.load_state_dict(load_state_dict_from_url(checkpoint, progress=True))
+    return model
